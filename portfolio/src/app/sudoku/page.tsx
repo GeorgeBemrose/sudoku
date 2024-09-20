bbe825fdc7e562f4f7457550b5b0be6ce57f48dd
GeorgeBemrose <GeorgeBemrose@gmail.com>
2024-09-20 09:05:52 +0100

    created board and button, need to handleChange of individual box

--- portfolio/src/app/sudoku/page.tsx
+++ portfolio/src/app/sudoku/page.tsx
@@ -1,101 +1,47 @@
-import Image from "next/image";
+'use client'
+
+import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { Button, Grid2, TextField } from '@mui/material';
+import { Container } from '@mui/system';
+import { useState } from 'react';
+
+//I want a 3x3 grid of 3x3 squares
+//I want to be able to enter a number into each of the squares
+//some squares are pre-populated
+//I want a submit button which validates the game is done
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // let sudoku = [[0,0,4,0,5,0,9,7,3],[2,0,7,4,6,0,0,0,0],[8,3,0,1,0,0,4,0,0],[0,0,0,0,8,0,0,0,6],[0,2,6,5,7,1,0,4,0],[0,1,3,0,0,0,2,5,0],[8,9,0,4,0,0,6,0,5],[0,0,2,0,1,8,0,0,4],[0,0,7,5,6,0,0,0,1]]
+  let [sudoku,setSudoku] = useState([[0,0,4,0,5,0,9,7,3],[2,0,7,4,6,0,0,0,0],[8,3,0,1,0,0,4,0,0],[0,0,0,0,8,0,0,0,6],[0,2,6,5,7,1,0,4,0],[0,1,3,0,0,0,2,5,0],[8,9,0,4,0,0,6,0,5],[0,0,2,0,1,8,0,0,4],[0,0,7,5,6,0,0,0,1]]);
+  
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+  const handleChange = (event) => {
+    setSudoku(event.target.value)
+  }
+  
+  return (<>
+  <Container maxWidth="sm">
+    <p className="text-3xl justify-center">Sudoku</p>
+
+      <Box className="grid grid-cols-3 grid-rows-3">
+        {sudoku.map((i,indexi) => (
+            <div key={indexi} className="grid grid-cols-3 grid-rows-3 p-2">
+              {i.map((j,indexj) => (
+                <TextField onChange={handleChange} key={indexi+","+indexj} value={j === 0 ? '' : j} size='small' disabled={j != 0} className="size-10"/>
+              ))}
+            </div>
+        ))}
+      </Box>
+      <Box className="w-full flex justify-end">
+        <Button>Submit</Button>
+      </Box>
+      
+      </Container>
+  </>
   );
 }
